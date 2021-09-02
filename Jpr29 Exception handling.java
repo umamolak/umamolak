8229b8bc0a7f023712a8234d9863fd3cc328dd0f
umamolak <88925171+umamolak@users.noreply.github.com>
2021-09-02 09:50:24 +0530

    Create Jpr29 Exception handling.java

---
+++ Jpr29 Exception handling.java
@@ -0,0 +1,26 @@
+
+package com.google;
+/*
+ * Author: Uma Molak 
+ *code to demonstrate exception handling
+ *
+ * */
+  
+    class JavaException {
+      public static void main(String args[]) {
+      int d = 0;
+      int n = 20;
+  try {
+   
+    int fraction = n / d;
+   
+   System.out.println("This line will not be Executed");
+  } 
+   catch (ArithmeticException e) {
+   
+   System.out.println("In the catch Block due to Exception = " + e);
+  }
+  
+   System.out.println("End Of Main");
+ }
+}
